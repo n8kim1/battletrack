9ba311f560fb2555b48b9011c61f3570938f58b5
n8kim1 <n8kim1@yahoo.com>
2024-05-27 22:07:42 -0400

    Impl loading a game

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
+import Game from '../../src/playback/Game'
+
+// TODO method from id to url of game source, using the helper method
+// or this can be integrated w tournament methods later right
+const replayId = '930826dd-74ba-45a2-806f-d6578b549a6a'
+const gameSource = `https://storage.googleapis.com/mitbattlecode-production-secure/episode/bc24/replays/${replayId}.bc24`
+
 async function main() {
+    const fetchRemoteGame = await fetch(gameSource)
+        .then((response) => response.arrayBuffer())
+        .then((buffer) => {
+            if (buffer.byteLength === 0) {
+                alert('Error: Game file is empty.')
+                throw Error
+            }
+            return buffer
+        })
+
+    const buffer = fetchRemoteGame
+    var game = Game.loadFullGameRaw(buffer)
+    console.log('Analyzing match from', gameSource)
 }
 
 main().catch((reason) => {
