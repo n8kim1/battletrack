75f99d7e9bc280f809e5a3a969d0f65c7bcd2aab
n8kim1 <n8kim1@yahoo.com>
2024-05-28 08:55:31 -0400

    refactor: Load matches from dynamic array

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
 import { Action } from 'battlecode-schema/js/battlecode/schema/action'
 import { ACTION_DEFINITIONS } from '../../src/playback/Actions'
 import Game from '../../src/playback/Game'
+import Match from '../../src/playback/Match'
+
 
 // TODO method from id to url of game source, using the helper method
 // or this can be integrated w tournament methods later right
 const replayId = '930826dd-74ba-45a2-806f-d6578b549a6a'
 const gameSource = `https://storage.googleapis.com/mitbattlecode-production-secure/episode/bc24/replays/${replayId}.bc24`
 
-async function main() {
-    let levelsAggregate = [0, 0, 0]
+let matches: Match[] = []
 
-    const fetchRemoteGame = await fetch(gameSource)
+async function fetchRemoteGame(gameSource: string): Promise<Match[]> {
+    return fetch(gameSource)
         .then((response) => response.arrayBuffer())
         .then((buffer) => {
             if (buffer.byteLength === 0) {
                 alert('Error: Game file is empty.')
                 throw Error
             }
-            return buffer
+            var game = Game.loadFullGameRaw(buffer)
+            console.log('Analyzing match from', gameSource)
+            return game.matches
         })
+}
 
-    const buffer = fetchRemoteGame
-    var game = Game.loadFullGameRaw(buffer)
-    console.log('Analyzing match from', gameSource)
+async function main() {
+    matches = matches.concat(await fetchRemoteGame(gameSource))
 
-    for (const match of game.matches) {
+    let levelsAggregate = [0, 0, 0]
+
+    for (const match of matches) {
+        const game = match.game
+        const teams = game.teams
+
         let currentTurnNumber = -1
         let maxTurn = match.maxTurn
 
@@ -43,7 +52,6 @@ async function main() {
 
         // Analyze team stats per match
         const stats = match.currentTurn.stat
-        const teams = game.teams
 
         for (const team of teams) {
             const teamStat = stats.getTeamStat(team)
@@ -62,7 +70,7 @@ async function main() {
         }
     }
 
-    const levelsNormed = levelsAggregate.map((l) => Math.round((l / (game.matches.length * 2)) * 100))
+    const levelsNormed = levelsAggregate.map((l) => Math.round((l / (matches.length * 2)) * 100))
     console.log(
         `Specialization levels: ${levelsNormed[0]}% attack, ${levelsNormed[1]}% build, ${levelsNormed[2]}% heal`
     )
