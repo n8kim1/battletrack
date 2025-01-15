12ec334186338af587172956473d48c8c079d1d6
n8kim1 <n8kim1@yahoo.com>
2025-01-14 22:46:57 -0500

    fixup! Impl match downloading

---
+++ src/stats/match.ts
@@ -0,0 +1,12 @@
+export interface BattletrackMatch {
+    uuid: string
+    episode: string
+}
+
+// TODO this info should be stored in a csv, and parsed from there
+export const matches: BattletrackMatch[] = [
+    {
+        uuid: 'c55f0bca-2252-4d73-ad95-8a6a6c8994db',
+        episode: 'bc25java'
+    }
+]
