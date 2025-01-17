d16b80084ff6dcc65c8d46be30d9b368992c8ca6
n8kim1 <n8kim1@yahoo.com>
2025-01-16 20:38:50 -0500

    Frontend code does less game work

--- src/dashboard/pages/main-page.tsx
+++ src/dashboard/pages/main-page.tsx
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { fetchMatch } from '../../stats'
-import Game from '../../../../src/playback/Game'
+import { getMatchData } from '../../stats'
 
 export const MainPage: React.FC = () => {
-    const [dataInState, updateState] = useState(0)
+    let spawnsEachTypeInit = ''
+
+    const [spawnsEachTypeState, updateState] = useState(spawnsEachTypeInit)
 
     async function loadMatchBuffers() {
-        const matchArrayBuffer = await fetchMatch()
-        // TODO don't rule out 0; handle void properly
-        if (matchArrayBuffer) {
-            // Some other stuff here, using the renderer. maybe
-            const game = Game.loadFullGameRaw(matchArrayBuffer)
-            console.log('game', game)
-            updateState(game.matches[0].maxRound)
+        const spawnsEachType = await getMatchData()
+        if (spawnsEachType) {
+            updateState(spawnsEachType)
         }
     }
 
@@ -20,5 +17,5 @@ export const MainPage: React.FC = () => {
         loadMatchBuffers()
     })
 
-    return <div>Game 1 has {dataInState} rounds</div>
+    return <div>The match has spawn quantities of: {spawnsEachTypeState}</div>
 }
