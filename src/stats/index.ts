b5ed9edeb3b18bd999ba8cd6ef15b9c3c011e77b
n8kim1 <n8kim1@yahoo.com>
2025-01-16 20:38:37 -0500

    Analyze data within a game file

--- src/stats/index.ts
+++ src/stats/index.ts
@@ -1,11 +1,45 @@
-// If I load this code from a broser context, can I use Game and Match?
-// import Game from '../../../src/playback/Game'
-// import Match from '../../../src/playback/Match'
+import Game from '../../../src/playback/Game'
+import Match from '../../../src/playback/Match'
 import { matches } from './match'
+import { RobotType, SpawnAction } from 'battlecode-schema/js/battlecode/schema'
 
-export function fetchMatch() {
-    // return matches.map((match) => {
-    // TODO check the pathing
+export function spawnsPerMatch(match: Match) {
+    let spawnsEachType = new Map<RobotType, number>()
+
+    console.log(match.currentRound, 'current round')
+    while (match.currentRound.roundNumber < match.maxRound) {
+        const currentRound = match.currentRound
+        const roundNumber = currentRound.roundNumber
+        if (roundNumber % 10 == 0) {
+            console.log('on round', currentRound.roundNumber)
+        }
+
+        // Jump to the end to populate all actions
+        currentRound.jumpToTurn(currentRound.turnsLength)
+        for (const action of match.currentRound.actions.actions) {
+            const actionData = action.actionData
+            if (actionData instanceof SpawnAction) {
+                const robotType = actionData.robotType()
+
+                // Hacky counter impl but w/e
+                let currentCount = spawnsEachType.get(robotType)
+                if (currentCount === undefined) {
+                    spawnsEachType.set(robotType, 0)
+                    currentCount = 0
+                }
+                spawnsEachType.set(robotType, currentCount + 1)
+            }
+        }
+        match._stepRound(1)
+    }
+
+    // To store in React state for display, need a more serializable / printable version
+    // TODO turn this into an array or smth, that can be manipulated easily by callers
+
+    return `${[...spawnsEachType]}`
+}
+
+export function getMatchData() {
     const match = matches[0]
     return fetch(`http://localhost:3000/battletrack/data/${match.uuid}.${match.episode}`)
         .then((response) => response.arrayBuffer())
@@ -14,86 +48,11 @@ export function fetchMatch() {
                 alert('Error: Game file is empty.')
                 throw Error
             }
-            console.log('buffer loaded, byteLength', buffer.byteLength)
-            return buffer
-            // var game = Game.loadFullGameRaw(buffer)
-            // console.log('Analyzing match from', gameSource)
-            // return game.matches
+            var game = Game.loadFullGameRaw(buffer)
+            const spawns = spawnsPerMatch(game.matches[0])
+
+            return spawns
         })
         .catch((e) => console.error(e))
     // })
 }
-
-// fetchRemoteGame(gameSource)
-
-// // TODO a method to save matches
-// // Then store into a helper fn
-// // All async stuff can run there, i guess
-// export async function main() {
-//     return 1
-// }
-// //     matches = matches.concat(await fetchRemoteGame(gameSource))
-
-// //     // let levelsAggregate = [0, 0, 0]
-
-// //     // for (const match of matches) {
-// //     //     const game = match.game
-// //     //     const teams = game.teams
-
-// //     //     let currentRoundNumber = -1
-// //     //     let maxRound = match.maxRound
-
-// //     //     while (currentRoundNumber < maxRound) {
-// //     //         match._stepRound(1)
-// //     //         currentRoundNumber = match.currentRound.roundNumber
-
-// //     //         // Analyze actions per round. (Can analyze bodies, etc, in a similar way)
-// //     //         const actions = match.currentRound.actions.actions
-// //     //         for (const action of actions) {
-// //     //             if (action instanceof SpawnAction) {
-// //     //                 console.log('SpawnAction found')
-// //     //             }
-// //     //         }
-// //     //     }
-
-// //     //     // Analyze team stats per match
-// //     //     const stats = match.currentRound.stat
-
-// //     //     // for (const team of teams) {
-// //     //     //     const teamStat = stats.getTeamStat(team)
-// //     //     //     const levels = teamStat.specializationTotalLevels
-
-// //     //     //     // Do some example math.
-// //     //     //     // (Aggregate _relative_ usage of each specialization,
-// //     //     //     // for consistent aggregation)
-// //     //     //     const levelsSlice = levels.slice(1, 4)
-// //     //     //     const levelsTotal = levelsSlice.reduce((a, b) => a + b)
-// //     //     //     const levelsNormed = levelsSlice.map((l) => l / levelsTotal)
-
-// //     //     //     for (const i in levelsAggregate) {
-// //     //     //         levelsAggregate[i] += levelsNormed[i]
-// //     //     //     }
-// //     //     // }
-// //     // }
-
-// //     // const levelsNormed = levelsAggregate.map((l) => Math.round((l / (matches.length * 2)) * 100))
-// //     // console.log(
-// //     //     `Specialization levels: ${levelsNormed[0]}% attack, ${levelsNormed[1]}% build, ${levelsNormed[2]}% heal`
-// //     // )
-
-// //     // return levelsNormed
-// //     return 1
-// // }
-
-// // main()
-// //     .catch((reason) => {
-// //         console.log('Error:', reason)
-// //     })
-// //     .then((value) => {
-// //         if (value) {
-// //             fs.writeFile('mynewfile3.txt', value.toString(), (err) => {
-// //                 if (err) throw err
-// //                 console.log('The file has been saved!')
-// //             })
-// //         }
-// //     })
